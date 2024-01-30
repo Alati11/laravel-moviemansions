a35aab47ea060de3fc52f94d1172eeda06d13e4b
Alati11 <peppealati@gmail.com>
2024-01-30 18:20:52 +0100

    ADD building migration

--- database/migrations/2024_01_30_155226_create_buildings_table.php
+++ database/migrations/2024_01_30_155226_create_buildings_table.php
@@ -13,6 +13,24 @@ return new class extends Migration
     {
         Schema::create('buildings', function (Blueprint $table) {
             $table->id();
+            $table->foreign('user_id')
+            ->references('id')
+            ->on('users')
+            ->onDelete('set null')
+            ->onUpdate('cascade');
+            $table->string('title');
+            $table->unsignedInteger('rooms');
+            $table->unsignedInteger('beds');
+            $table->unsignedInteger('bathrooms');
+            $table->unsignedInteger('sqm');
+            $table->unsignedInteger('latitude');
+            $table->unsignedInteger('longitude');
+            $table->text('description');
+            $table->string('address');
+            $table->string('image');
+            $table->boolean('available');
+            $table->string('slug')->unique();
+
             $table->timestamps();
         });
     }
