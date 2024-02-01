f2a98f4144fc8d498d41311048d4b6a4cfab3438
Anse98 <lorenzo.anselmi98.com>
2024-02-01 17:54:56 +0100

    ADD-Index view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,11 @@
 
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
+   
 
     <!-- Usando Vite -->
     @vite(['resources/js/app.js'])
@@ -45,6 +48,9 @@
                     <ul class="navbar-nav me-auto">
                         <li class="nav-item">
                             <a class="nav-link" href="{{url('/') }}">{{ __('Home') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('admin.buildings.index') }}">{{ __('Appartamenti') }}</a>
                         </li>
                     </ul>
 
