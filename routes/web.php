6ec6f1a91878e7d6ed7ebd545ebb1e7ad9974e2a
FedericaSidoti <federicasidoti21@gmail.com>
2024-02-01 13:09:03 +0100

    FIX- routes login  e dashboard

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth/login');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('admin/dashboard', function () {
+    return view('admin/dashboard');
+})->middleware(['auth', 'verified'])->name('admin.dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
