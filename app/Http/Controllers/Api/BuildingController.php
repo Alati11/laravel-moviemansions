a1dca8817472a73ee9f8645f2ddefe56cab3bfa3
igorliotta <liottaigor@gmail.com>
2024-02-08 10:09:15 +0100

    FIX- function index in Api/buildingcontroller

--- app/Http/Controllers/Api/BuildingController.php
+++ app/Http/Controllers/Api/BuildingController.php
@@ -8,17 +8,43 @@ use Illuminate\Http\Request;
 
 class BuildingController extends Controller
 {
+
     public function index(Request $request)
     {
+        $query = $request->input('search');
+        $lat = $request->input('lat');
+        $lon = $request->input('lon');
+        $rad = $request->input('rad');
+        $rooms = $request->input('rooms');
+        $beds = $request->input('beds');
+        $bathrooms = $request->input('bathrooms');
 
-        $results = Building::with('services', 'images', 'sponsorships')->paginate(10);
+        $queryBuilder = Building::query();
 
-        return response()->json([
-            'results' => $results,
-            'success' => true
-        ]);
+        if ($query) {
+            $queryBuilder->where('address', 'like', '%' . $query . '%');
+        }
+
+        if ($lat && $lon) {
+            $queryBuilder->whereRaw("ST_Distance_Sphere(point(longitude, latitude), point(?, ?)) <= ?", [$lon, $lat, $rad]);
+        }
+
+        if ($beds) {
+            $queryBuilder->where('beds', $beds);
+        }
+
+        if ($rooms) {
+            $queryBuilder->where('rooms', $rooms);
+        }
+
+        if ($bathrooms) {
+            $queryBuilder->where('bathrooms', $bathrooms);
+        }
+
+        $buildings = $queryBuilder->get();
+
+        return response()->json(['buildings' => $buildings]);
     }
-
     public function show(Building $building)
     {
         // $post = Post::with('category','tags')->where('slug',$slug)->first();
