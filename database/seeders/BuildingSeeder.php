445655743f3c6833d24b14d660244afe523c1056
FedericaSidoti <federicasidoti21@gmail.com>
2024-02-16 11:00:44 +0100

    fixing-id

--- database/seeders/BuildingSeeder.php
+++ database/seeders/BuildingSeeder.php
@@ -21,11 +21,11 @@ class BuildingSeeder extends Seeder
         $buildings = [
 
             [
-                'title' => 'Harry Potter’s childhood house',
-                'user_id' => 3,
+                'title' => 'Harry Potter: Privet Drive',
+                'user_id' => 9,
                 'rooms' => 15,
                 'beds' => 4,
-                'description' => 'This is the house used to represent the interiors of Privet Drive, Little Whinging, Surrey: that is to say, Harry Potter’s childhood home! For paying guests, there are two rooms with four beds each, a fireplace, and an inner garden, in addition to an English breakfast included with every stay.',
+                'description' => "Ecco la casa utilizzata per rappresentare gli interni di Privet Drive, Little Whinging, Surrey: ovvero, la casa d'infanzia di Harry Potter! Per gli ospiti paganti, ci sono due camere con quattro letti ciascuna, un camino e un giardino interno, oltre a una colazione inglese inclusa in ogni soggiorno.",
                 'bathrooms' => 2,
                 'sqm' => 150,
                 'address' => '12 Picket Post Close, Martins Heron, Bracknell, Berkshire, RG12 9BX, Regno Unito',
@@ -38,11 +38,11 @@ class BuildingSeeder extends Seeder
 
             [
 
-                'title' => 'Tony Stark lakeside cottage',
+                'title' => 'Tony Stark cottage sul lago',
                 'user_id' => 6,
                 'rooms' => 9,
                 'beds' => 2,
-                'description' => "If your dream is to live like an Avenger in  every way, there's a novelty that could make it partly come  true. In fact, for some time now, it has been possible to rent  the bungalow where Tony Stark retired at the beginning of  Avengers: Endgame. It's a lakeside cabin, a wooden construction  resembling a bungalow overlooking a peaceful lake. Located in  the heart of Bouckart Farm, just a 30-minute drive from  Atlanta, the residence offers three bedrooms and three  bathrooms and can accommodate up to six people.",
+                'description' => "Se il tuo sogno è vivere come un Avenger in ogni modo, c'è una novità che potrebbe renderlo in parte realtà. Infatti, da qualche tempo è possibile affittare il bungalow dove Tony Stark si ritirò all'inizio di Avengers: Endgame. Si tratta di una cabina sul lago, una costruzione in legno simile a un bungalow con vista su un lago tranquillo. Situata nel cuore di Bouckart Farm, a soli 30 minuti di auto da Atlanta, la residenza offre tre camere da letto e tre bagni e può ospitare fino a sei persone.",
                 'bathrooms' => 2,
                 'sqm' => 100,
                 'address' => '9445 Browns Lake Rd, Fairburn, GA',
@@ -55,11 +55,11 @@ class BuildingSeeder extends Seeder
             ],
 
             [
-                'title' => 'Home alone: the home',
+                'title' => "Mamma ho perso l'aereo: la casa",
                 'user_id' => 5,
                 'rooms' => 22,
                 'beds' => 4,
-                'description' => "The building has many spacious rooms, perfect  for Christmas decorations and funny traps. The house is the one  made famous by the movie 'Home Alone' directed by Chris  Columbus. There are four bedrooms, as well as four bathrooms,  making for a total area of 400 square meters.",
+                'description' => "La villa ha molte camere spaziose, perfette per decorazioni natalizie e buffe trappole. La casa in questione è quella resa famosa dal film «Mamma, ho perso l’aereo» di Chris Columbus. Le stanze da letto sono quattro, come quattro sono i bagni, per una superficie totale di 400 metri quadrati.",
                 'bathrooms' => 5,
                 'sqm' => 400,
                 'address' => '671 Lincoln Avenue, Winnetka, IL 60093-2345',
@@ -71,11 +71,11 @@ class BuildingSeeder extends Seeder
             ],
 
             [
-                'title' => "Mrs Doubtfire's perfect house",
+                'title' => "La perfetta casa di Mrs Doubtfire",
                 'user_id' => 4,
                 'rooms' => 16,
                 'beds' => 2,
-                'description' => "A splendid Victorian house in San Francisco, California,which still today attracts fans of the late Robin Williams. This is where they filmed 'Mrs. Doubtfire.' The house features 4 bedrooms and 4 bathrooms arranged over 300 square meters.",
+                'description' => "Una splendida casa vittoriana a San Francisco, California che, ancora oggi, è meta dei fan del compianto Robin Williams. Qui hanno girato «Mrs. Doubtfire – Mammo per sempre». La casa offre 4 camere da letto e 4 bagni disposti su 300 metri quadrati.",
                 'bathrooms' => 4,
                 'sqm' => 300,
                 'address' => '2640 Steiner Street, San Francisco, California CA',
@@ -87,11 +87,11 @@ class BuildingSeeder extends Seeder
             ],
 
             [
-                'title' => 'Padme Palace (Casino included)',
-                'user_id' => 8,
+                'title' => 'Palazzo Padmè (Casinò incuso)',
+                'user_id' => 9,
                 'rooms' => 86,
                 'beds' => 12,
-                'description' => "We're not in Naboo, but on the Como Lake, and this isn't the palace of Queen Padmé Amidala (who marries Anakin Skywalker right here), but Villa del Balbianello in Lenno, now a heritage site of FAI (Italian Environmental Fund). It has also been one of the locations in '007 - Casino Royale.",
+                'description' => "Non siamo a Naboo, ma sul lago di Como, e questo non è il palazzo della regina Padmé Amidala (che proprio qui sposa ad Anakin Skywalker) ma Villa del Balbianello a Lenno, oggi patrimonio del Fai. È stata una delle location anche di «007-Casino Royale».",
                 'bathrooms' => 22,
                 'sqm' => 1500,
                 'address' => '22016 Tremezzina CO',
@@ -103,11 +103,11 @@ class BuildingSeeder extends Seeder
             ],
 
             [
-                'title' => 'Welcome to Hogwarts (archery field added)',
+                'title' => 'Hogwarts (e foresta di Sherwood)',
                 'user_id' => 7,
                 'rooms' => 150,
                 'beds' => 110,
-                'description' => "Maybe you won't learn to fly on a broom, but Hogwarts Castle does exist. The name of the famous structure is actually Alnwick Castle. Before Harry Potter, the castle had already been selected as a set for other films like Robin Hood: Prince of Thieves. The building boasts around 150 rooms, a multitude of beds, and a corridor truly filled with armors – just beware of the second-floor bathroom, Moaning Myrtle doesn't like visitors.",
+                'description' => "Magari non imparerai a volare su una scopa, ma il Castello di Hogwarts esiste davvero. il nome del celeberrimo castello di Hogwarts in realtà è Alnwick Castle. Prima di Harry Potter, il castello era già stato selezionato come set in altri film come Robin Hood - Principe dei ladri. La struttura conta circa 150 stanze, una moltitudine di letti e un corridoio realmente pieno di armature – attenti solo al bagno del secondo piano, Mirtilla malcontenta non ama i visitatori.",
                 'bathrooms' => 32,
                 'sqm' => 217.800,
                 'address' => 'Alnwick NE66 1NQ, Regno Unito',
@@ -119,11 +119,11 @@ class BuildingSeeder extends Seeder
             ],
 
             [
-                'title' => 'Home of Walter White – Location Breaking Bad',
+                'title' => 'Casa di Walter White: Breaking Bad',
                 'user_id' => 1,
                 'rooms' => 9,
                 'beds' => 3,
-                'description' => "The house of Walter White is the ideal destination for every Breaking Bad fan. Located in Albuquerque, New Mexico, it is one of the most iconic locations in the series, where some of the key and most intense scenes take place. It is the residence where Walter lives with his wife Skyler, son Walter Junior, and little Holly.",
+                'description' => "La casa di Walter White è la meta ideale per ogni fan di Breaking Bad. Situata ad Albuquerque, nel New Mexico, è uno dei luoghi più iconici della serie, in cui si svolgono alcune delle scene chiave e più intense. È la dimora dove Walter vive con la moglie Skyler, il figlio Walter Junior e la piccola Holly.",
                 'bathrooms' => 2,
                 'sqm' => 150,
                 'address' => '3828 Piermont Dr NE, Albuquerque, NM 87111',
@@ -135,11 +135,14 @@ class BuildingSeeder extends Seeder
             ],
 
             [
-                'title' => 'Home Cohen - The O.C.',
-                'user_id' => 2,
+                'title' => 'Casa Cohen - The O.C.',
+                'user_id' => 9,
                 'rooms' => 16,
                 'beds' => 6,
-                'description' => "The Cohen house is an elegant two-story building with a Mediterranean architectural style. The house is spacious, featuring open and modern furnishings with carefully crafted architectural details. The living room is welcoming. The backyard is also a focal point, with a pool and a lounge area, reflecting the luxury and comfort of life in Orange County, California. The Cohen house becomes a recognizable element in the series, contributing to the visual character and representation of the world in which the characters live.",
+                'description' => "La casa dei Cohen è un elegante edificio di due piani con uno stile architettonico mediterraneo.
+                L’abitazione è piuttosto grande e vanta 6,376 metri quadrati, 6 camere da letto e 6 bagni.
+                Gli interni presentano tonalità tenui e neutre, con complementi d’arredo decisamente moderni.
+                Il cortile posteriore è un punto focale, con una piscina e una zona lounge, riflettendo il lusso e il comfort della vita nella contea di Orange, California.",
                 'bathrooms' => 6,
                 'sqm' => 350,
                 'address' => '6205 Ocean Breeze Drive, Malibu.',
@@ -151,11 +154,11 @@ class BuildingSeeder extends Seeder
             ],
 
             [
-                'title' => 'Creel House  - Stranger Things',
-                'user_id' => 5,
+                'title' => 'Casa Creel - Stranger Things',
+                'user_id' => 9,
                 'rooms' => 24,
                 'beds' => 7,
-                'description' => "Located in the city of Rome, Georgia, more precisely at 906 East 2nd Avenue, and built in 1882, this is an old Victorian-style residence boasting a surface area of 460 square meters. It features five bedrooms, five bathrooms, eight fireplaces, an attic, and an annex with two bedrooms and a bathroom. Not only that, but it also includes an extensive surrounding land.",
+                'description' => "Situata nella città di Rome, in Georgia, più precisamente nella 906 East 2nd Avenue e costruita nel 1882, si tratta di una vecchia dimora in stile vittoriano che può vantare una superficie di 460 metri quadrati e conta cinque stanze da letto, cinque bagni, otto camini, un attico e una dependance con due camere da letto e un bagno. Non solo, conta anche un vasto terreno circostante.",
                 'bathrooms' => 6,
                 'sqm' => 460,
                 'address' => '906 East 2nd Avenue, Rome, Giorgia.',
@@ -167,11 +170,11 @@ class BuildingSeeder extends Seeder
             ],
 
             [
-                'title' => 'The House of Jimmy  - Pulp Fiction',
+                'title' => 'Casa di Jimmy  - Pulp Fiction',
                 'user_id' => 8,
                 'rooms' => 8,
                 'beds' => 3,
-                'description' => "The house measures 158 square meters and internally comprises 3 bedrooms and 2 bathrooms. But Pulp Fiction fans will not forget two other spaces used in that sequence. Firstly, the garage, then the garden. In the garage, the car was parked, and the body of the victim was cleaned. In the garden, Vincent and Jules, completely naked, are forced to wash themselves with the cold water hose used for watering the flowers.",
+                'description' => "La casa misura 158 metri quadrati e comprende internamente 3 camere da letto e 2 bagni. Ma i fan di Pulp Fiction non potranno scordare altri due ambienti, utilizzati in quella sequenza. Innanzitutto il garage, poi il giardino. Nel primo l'auto fu parcheggiata l’auto e ripulita con il corpo della vittima. Nel secondo Vicent e Jules, completamente nudi, sono obbligati a lavarsi con il tubo di acqua fredda usato per innaffiare i fiori.",
                 'bathrooms' => 2,
                 'sqm' => 158,
                 'address' => '414 Kraft Avenue, Studio City, CA 91604.',
@@ -215,8 +218,8 @@ class BuildingSeeder extends Seeder
             ],
 
             [
-                'title' => 'Jep Ganbardella House',
-                'user_id' => 4,
+                'title' => 'Jep Gambardella House',
+                'user_id' => 9,
                 'rooms' => 17,
                 'beds' => 8,
                 'description' => "Jep's house is a wonderful attic in Piazza del Colosseo, Rome, a majestic red building, to be precise at number 9. You can enjoy the white entrance hall, with imperial columns and important marbles. Very famous and mentioned in Sorrentino's Oscar-winning film 'The Great Beauty'.",
@@ -232,7 +235,7 @@ class BuildingSeeder extends Seeder
 
             [
                 'title' => 'Hatfield House: apparently, Hollywood’s favourite set',
-                'user_id' => 8,
+                'user_id' => 9,
                 'rooms' => 223,
                 'beds' => 200,
                 'description' => "With its 223 rooms, as reported by Tatler, Hatfield House has served as a backdrop for various films and TV series, over seventy in total, captivating directors and set designers. Just in 2020, Hatfield House appeared in highly successful films such as 'Enola Holmes' with Millie Bobby Brown, 'Bridgerton' and 'The Crown,' but in previous years, it had also hosted 'Lara Croft: Tomb Raider,' 'V for Vendetta,' multiple Batman films, and 'Mortdecai,' as well as in several films directed by Tim Burton, including 'Charlie and the Chocolate Factory' and 'Sleepy Hollow.",
