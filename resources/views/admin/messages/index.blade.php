3cab9e14ab6675895f8352c66041cae1d6396cba
FedericaSidoti <federicasidoti21@gmail.com>
2024-02-18 09:46:42 +0100

    fix-no building messages

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -8,7 +8,7 @@
             <small class="title-msg">Messaggi</small>
         </div>
     </div>
-    
+    @if ($buildings->count() !== 0)
     @foreach ($buildings as $building)
         <div class="building-wrap">
             <h4 class="accordion-title">
@@ -61,6 +61,27 @@
             @endif
         </div>
     @endforeach
+    @else
+    <div class="col-9 d-flex flex-column justify-content-center align-items-center py-5">
+        <div class="info mb-5 info-no-buildings">
+            <div class="info__icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" height="24" fill="none"><path fill="#393a37" d="m12 1.5c-5.79844 0-10.5 4.70156-10.5 10.5 0 5.7984 4.70156 10.5 10.5 10.5 5.7984 0 10.5-4.7016 10.5-10.5 0-5.79844-4.7016-10.5-10.5-10.5zm.75 15.5625c0 .1031-.0844.1875-.1875.1875h-1.125c-.1031 0-.1875-.0844-.1875-.1875v-6.375c0-.1031.0844-.1875.1875-.1875h1.125c.1031 0 .1875.0844.1875.1875zm-.75-8.0625c-.2944-.00601-.5747-.12718-.7808-.3375-.206-.21032-.3215-.49305-.3215-.7875s.1155-.57718.3215-.7875c.2061-.21032.4864-.33149.7808-.3375.2944.00601.5747.12718.7808.3375.206.21032.3215.49305.3215.7875s-.1155.57718-.3215.7875c-.2061.21032-.4864.33149-.7808.3375z"></path></svg>
+            </div>
+            <div class="info__title">
+                <b>Non hai ancora registrato nessun appartamento!</b>
+            </div>
+        </div>
+
+        <a href="{{route('admin.buildings.create')}}">
+            <button class="register-btn">
+                Registrane uno
+                <div class="arrow-wrapper">
+                    <div class="arrow"></div>
+                </div>
+            </button>
+        </a>       
+    </div>
+    @endif
 
 
    
